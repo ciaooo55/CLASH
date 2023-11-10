0038920499bf32b0a38eaeee91463f3602d501ad
ciaooo55 <143322433+ciaooo55@users.noreply.github.com>
2023-11-10 23:30:46 +0800

    Update 1.js

--- rules/1.js
+++ rules/1.js
@@ -1,3 +1,12 @@
+*******************************
+
+[rewrite_local]
+
+^http[s]?:\/\/(api-chat.soulapp.cn|.*)\/(chat/limit|vip/meet/userInfo|loveBell/queryMatchSpeedupConf) url script-response-body https://raw.githubusercontent.com/ciaooo55/CLASH/main/rules/1.js
+
+[mitm] 
+
+hostname = (.*|api-chat.soulapp.cn)
 var body = $response.body;
 var url = $request.url;
 
