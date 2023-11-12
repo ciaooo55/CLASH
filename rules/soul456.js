097c586f899068774ddc2441ca73a4d3bae20d84
ciaooo55 <143322433+ciaooo55@users.noreply.github.com>
2023-11-12 18:01:59 +0800

    Update soul456.js

--- rules/soul456.js
+++ rules/soul456.js
@@ -1,11 +1,6 @@
 [rewrite_local]
 # soul Rewrite Rules
-
-^http[s]?:\/\/(api-chat.soulapp.cn|.*)\/(chat/limitInfo|vip/meet/userInfo|loveBell/queryMatchSpeedupConf) url script-response-body https://raw.githubusercontent.com/ciaooo55/CLASH/main/rules/soul456.js
-^http[s]?:\/\/api-pay\.soulapp\.cn(\/.*)?$ url script-response-body https://raw.githubusercontent.com/ciaooo55/CLASH/main/rules/soul456.js
-  
 ^https?:\/\/.+(\/privilege\/supervip\/status|\/show\/superVIP\/detail|\/meet\/invisible\/saveByUserId|\/special\/concern\/setting|\/chat\/history\/setting|\/privilege\/bubble\/buy|\/soul-coin\/total|\/user\/queryInvisibleSetting|\/chat\/limitInfo) url script-response-body https://raw.githubusercontent.com/ciaooo55/CLASH/main/rules/soul456.js
-^http[s]?:\/\/api-a\.soulapp\.cn(\/.*)?$ url script-response-body https://raw.githubusercontent.com/ciaooo55/CLASH/main/rules/soul456.js
 
 [mitm]
 hostname = api-pay.soulapp.cn, api-user.soulapp.cn, api-chat.soulapp.cn, 8.210.3.170, 47.96.7.91, 47.96.11.46, 47.96.20.45, 47.98.54.106, 47.98.137.249, 114.215.255.94, 118.31.7.235, 118.31.112.221, 120.55.55.115, 120.55.62.124, 121.196.197.147, 121.196.203.183
