7df398af39fcd3cb407aab85b0bbc637853693a9
ciaooo55 <143322433+ciaooo55@users.noreply.github.com>
2023-11-12 18:31:14 +0800

    Update soul456.js

--- rules/soul456.js
+++ rules/soul456.js
@@ -6,47 +6,38 @@
 hostname = api-pay.soulapp.cn, api-user.soulapp.cn, api-chat.soulapp.cn, 8.210.3.170, 47.96.7.91, 47.96.11.46, 47.96.20.45, 47.98.54.106, 47.98.137.249, 114.215.255.94, 118.31.7.235, 118.31.112.221, 120.55.55.115, 120.55.62.124, 121.196.197.147, 121.196.203.183
 
 *******************************/
+    
 var body = $response.body;
 var urlq = $request.url;
 var objc = JSON.parse(body);
 
-const hausd0rff = {"code": 10001, "message": "弹窗，请支持我们的工作", "jiajia": true};
+// 从 XML 中提取的 Rewrite 规则
+const rewriteRules = [
+    {"matchValue": "\"limit\":true", "newValue": "\"limit\":false"},
+    {"matchValue": "\"superVIP\":false", "newValue": "\"superVIP\":true"},
+    {"matchValue": "\"validTime\":null", "newValue": "\"validTime\":4567891456000"},
+    {"matchValue": "\"hasFlyPackage\":false", "newValue": "\"hasFlyPackage\":true"},
+    {"matchValue": "\"speedup\":false", "newValue": "\"speedup\":true"}
+];
 
-if (urlq.indexOf("/profile/settings") != -1) {
-    objc["userVIP"]["superVIP"] = true;
-    objc["userVIP"]["hasMyMeet"] = true;
-    objc["userVIP"]["isCanSendVideo"] = true;
-    objc["userVIP"]["isCanSendVoice"] = true;
-    objc["userVIP"]["limit"] = false;
-    objc["userVIP"]["hasFlyPackage"] = false;
-    objc["userVIP"]["validTime"] = 999999;
-    objc["userVIP"]["superVIPDays"] = 999999;
-}
+// 应用 Rewrite 规则
+rewriteRules.forEach(rule => {
+    if (rule.matchRequest && urlq.indexOf(rule.matchValue) !== -1) {
+        body = body.replace(new RegExp(rule.matchValue, "g"), rule.newValue);
+    } else if (rule.matchResponse) {
+        body = body.replace(new RegExp(rule.matchValue, "g"), rule.newValue);
+    }
+});
 
-if (urlq.indexOf("/private/buy") != -1) {
-    objc["userVIP"]["superVIP"] = true;
-    objc["userVIP"]["hasMyMeet"] = true;
-    objc["userVIP"]["showSuperVIP"] = true;
-    objc["userVIP"]["limit"] = false;
-    objc["userVIP"]["validTime"] = 999999;
-    objc["userVIP"]["superVIPDays"] = 4092599349000;
-}
+$done({body: body});
 
-if (urlq.indexOf("/chat/history/setting") != -1) {
-    objc["userVIP"] = 999999;
-}
 
-if (urlq.indexOf("/settings/contact") != -1) {
-    objc = hausd0rff;
-}
 
-if (urlq.indexOf("/chat/limit") != -1) {
-    objc = {"code": 10001, "message": "弹窗，请支持我们的工作", "data": {"success": true, "code": 10001}, "success": true};
-}
 
-if (urlq.indexOf("/user/query") != -1) {
-    objc = hausd0rff;
-}
 
-$done({body: JSON.stringify(objc)});
+
+
+
+
+
 
