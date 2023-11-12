101d077a0fa71bb7667453e26fd9f5cee3c49865
ciaooo55 <143322433+ciaooo55@users.noreply.github.com>
2023-11-12 22:19:59 +0800

    Update 1.js

--- rules/1.js
+++ rules/1.js
@@ -9,21 +9,20 @@ hostname = api-chat.soulapp.cn, api-pay.soulapp.cn, api-a.soulapp.cn
 
 *******************************/
 var body = $response.body;
-var url = $request.url;
 
-// 修改 "limit":true 为 "limit":false
+// 将所有匹配到的 "limit":true 替换为 "limit":false
 body = body.replace(/"limit":true/g, '"limit":false');
 
-// 修改 "superVIP":false 为 "superVIP":true
+// 将所有匹配到的 "superVIP":false 替换为 "superVIP":true
 body = body.replace(/"superVIP":false/g, '"superVIP":true');
 
-// 修改 "validTime":null 为 "validTime":4567891456000
+// 将所有匹配到的 "validTime":null 替换为 "validTime":4567891456000
 body = body.replace(/"validTime":null/g, '"validTime":4567891456000');
 
-// 修改 "hasFlyPackage":false 为 "hasFlyPackage":true
+// 将所有匹配到的 "hasFlyPackage":false 替换为 "hasFlyPackage":true
 body = body.replace(/"hasFlyPackage":false/g, '"hasFlyPackage":true');
 
-// 修改 "speedup":false 为 "speedup":true
+// 将所有匹配到的 "speedup":false 替换为 "speedup":true
 body = body.replace(/"speedup":false/g, '"speedup":true');
 
 $done({ body });
