8c8adcdf9e7a822728c746fe891d363d5cd7228a
ciaooo55 <143322433+ciaooo55@users.noreply.github.com>
2023-11-12 21:52:45 +0800

    Update 123.js

--- rules/123.js
+++ rules/123.js
@@ -5,7 +5,6 @@
 hostname = api-pay.soulapp.cn, api-user.soulapp.cn, api-chat.soulapp.cn, 8.210.3.170, 47.96.7.91, 47.96.11.46, 47.96.20.45, 47.98.54.106, 47.98.137.249, 114.215.255.94, 118.31.7.235, 118.31.112.221, 120.55.55.115, 120.55.62.124, 121.196.197.147, 121.196.203.183
 
 *******************************/
-var urlq = $request.url;
 var body = $response.body;
 
 // 修改 "limit":true 为 "limit":false
@@ -24,14 +23,3 @@ body = body.replace(/"hasFlyPackage":false/g, '"hasFlyPackage":true');
 body = body.replace(/"speedup":false/g, '"speedup":true');
 
 $done({ body });
-
-
-
-
-
-
-
-
-
-
-
