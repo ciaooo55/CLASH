8e8a0889ef2aeb7e9e93a2d9b369db419084544c
ciaooo55 <143322433+ciaooo55@users.noreply.github.com>
2023-11-12 21:40:58 +0800

    Update 123.js

--- rules/123.js
+++ rules/123.js
@@ -6,37 +6,25 @@ hostname = api-pay.soulapp.cn, api-user.soulapp.cn, api-chat.soulapp.cn, 8.210.3
 
 *******************************/
 var body = $response.body;
-
 var urlq = $request.url;
 var objc = JSON.parse(body);
 
-if ($request.url.includes('/chat/limitInfo')) {
-    body = body.replace(/"limit":true/g, '"limit":false');
-}
 
-// 替换 other_endpoint
-if ($request.url.includes('/privilege/supervip/status') || 
-    $request.url.includes('/show/superVIP/detail') || 
-    $request.url.includes('/meet/invisible/saveByUserId') || 
-    $request.url.includes('/special/concern/setting') || 
-    $request.url.includes('/chat/history/setting') || 
-    $request.url.includes('/privilege/bubble/buy') || 
-    $request.url.includes('/soul-coin/total') || 
-    $request.url.includes('/user/queryInvisibleSetting')) {
-    body = body
-        .replace(/"superVIP":false/g, '"superVIP":true')
-        .replace(/"validTime":null/g, '"validTime":4567891456000')
-        .replace(/"hasFlyPackage":false/g, '"hasFlyPackage":true');
-}
+// 修改 "limit":true 为 "limit":false
+body = body.replace(/"limit":true/g, '"limit":false');
 
-// 替换 yet_another_endpoint
-if ($request.url.includes('/chat/limitInfo')) {
-    body = body
-        .replace(/"currentSpeed":\d+/g, '"currentSpeed":0')
-        .replace(/"speedupTime":\d+/g, '"speedupTime":0')
-        .replace(/"speedupCount":1/g, '"speedupCount":0')
-        .replace(/"speedup":false/g, '"speedup":true');
-}
+// 修改 "superVIP":false 为 "superVIP":true
+body = body.replace(/"superVIP":false/g, '"superVIP":true');
+
+// 修改 "validTime":null 为 "validTime":4567891456000
+body = body.replace(/"validTime":null/g, '"validTime":4567891456000');
+
+// 修改 "hasFlyPackage":false 为 "hasFlyPackage":true
+body = body.replace(/"hasFlyPackage":false/g, '"hasFlyPackage":true');
+
+// 修改 "speedup":false 为 "speedup":true
+body = body.replace(/"speedup":false/g, '"speedup":true');
 
 $done({ body });
+
 
