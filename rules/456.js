f3a6aa1c08ea2186d381d368e29f04088c33cbac
ciaooo55 <143322433+ciaooo55@users.noreply.github.com>
2023-11-13 16:35:09 +0800

    Update 456.js

--- rules/456.js
+++ rules/456.js
@@ -15,7 +15,7 @@ hostname = api-chat.soulapp.cn, api-pay.soulapp.cn, api-a.soulapp.cn
 
 *******************************/
 if ($request.url.includes('/chat/limitInfo')) {
-    body = body.replace(/"limit":true/g, '"limit":false');
+    $response.body = $response.body.replace(/"limit":true/g, '"limit":false');
 }
 
 // 替换 other_endpoint
@@ -27,7 +27,7 @@ if ($request.url.includes('/privilege/supervip/status') ||
     $request.url.includes('/privilege/bubble/buy') || 
     $request.url.includes('/soul-coin/total') || 
     $request.url.includes('/user/queryInvisibleSetting')) {
-    body = body
+    $response.body = $response.body
         .replace(/"superVIP":false/g, '"superVIP":true')
         .replace(/"validTime":null/g, '"validTime":4567891456000')
         .replace(/"hasFlyPackage":false/g, '"hasFlyPackage":true');
@@ -35,11 +35,12 @@ if ($request.url.includes('/privilege/supervip/status') ||
 
 // 替换 yet_another_endpoint
 if ($request.url.includes('/chat/limitInfo')) {
-    body = body
+    $response.body = $response.body
         .replace(/"currentSpeed":\d+/g, '"currentSpeed":0')
         .replace(/"speedupTime":\d+/g, '"speedupTime":0')
         .replace(/"speedupCount":1/g, '"speedupCount":0')
         .replace(/"speedup":false/g, '"speedup":true');
 }
 
-$done({ body });
+$done({});
+
