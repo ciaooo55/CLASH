a50478a72960f92a500c21c02ceb0e09afc09aaa
ciaooo55 <143322433+ciaooo55@users.noreply.github.com>
2023-11-13 17:07:01 +0800

    Update 456.js

--- rules/456.js
+++ rules/456.js
@@ -14,21 +14,28 @@ hostname = api-chat.soulapp.cn, api-pay.soulapp.cn, api-a.soulapp.cn
 
 *******************************/
 
-// 替换 endpoint
-if ($request.url.includes('soulapp.cn')) {
-    // 替换 other_endpoint
+
+if $request.url.includes('/privilege/supervip/status') || 
+    $request.url.includes('/show/superVIP/detail') || 
+    $request.url.includes('/meet/invisible/saveByUserId') || 
+    $request.url.includes('/special/concern/setting') || 
+    $request.url.includes('/chat/history/setting') || 
+    $request.url.includes('/privilege/bubble/buy') || 
+    $request.url.includes('/soul-coin/total') || 
+    $request.url.includes('/user/queryInvisibleSetting') || 
+    $request.url.includes('/chat/limitInfo')) {
     $response.body = $response.body
         .replace(/"superVIP":false/g, '"superVIP":true')
         .replace(/"validTime":null/g, '"validTime":4567891456000')
-        .replace(/"hasFlyPackage":false/g, '"hasFlyPackage":true');
-
-    // 替换 yet_another_endpoint
-    $response.body = $response.body
+        .replace(/"hasFlyPackage":false/g, '"hasFlyPackage":true')
+        .replace(/"limit":true/g, '"limit":false')
         .replace(/"currentSpeed":\d+/g, '"currentSpeed":0')
         .replace(/"speedupTime":\d+/g, '"speedupTime":0')
         .replace(/"speedupCount":1/g, '"speedupCount":0')
-        .replace(/"speedup":false/g, '"speedup":true')
-        .replace(/"limit":true/g, '"limit":false');
+        .replace(/"speedup":false/g, '"speedup":true');;
 }
 
+
+
 $done({});
+
