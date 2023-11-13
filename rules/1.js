abd241e86110e0ef21ac0c43b433c85bef39be8c
ciaooo55 <143322433+ciaooo55@users.noreply.github.com>
2023-11-13 15:49:20 +0800

    Update 1.js

--- rules/1.js
+++ rules/1.js
@@ -8,21 +8,18 @@
 hostname = api-chat.soulapp.cn, api-pay.soulapp.cn, api-a.soulapp.cn
 
 *******************************/
-var body = $response.body;
+var objc = JSON.parse($response.body);
+    objc = {
+ "data": {
+  "limit": false,
+  "superVIP": true,
+  "speedup": true,
+  "validTime": 4567891456000,
+  "hasFlyPackage": true,
+ },
+ "code": 0,
+ "msg": "返回成功"
+};
+$done({body : JSON.stringify(objc)});
 
-// 将所有匹配到的 "limit":true 替换为 "limit":false
-body = body.replace(/"limit":true/g, '"limit":false');
 
-// 将所有匹配到的 "superVIP":false 替换为 "superVIP":true
-body = body.replace(/"superVIP":false/g, '"superVIP":true');
-
-// 将所有匹配到的 "validTime":null 替换为 "validTime":4567891456000
-body = body.replace(/"validTime":null/g, '"validTime":4567891456000');
-
-// 将所有匹配到的 "hasFlyPackage":false 替换为 "hasFlyPackage":true
-body = body.replace(/"hasFlyPackage":false/g, '"hasFlyPackage":true');
-
-// 将所有匹配到的 "speedup":false 替换为 "speedup":true
-body = body.replace(/"speedup":false/g, '"speedup":true');
-
-$done({ body });
