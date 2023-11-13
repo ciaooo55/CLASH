554f34754339ca2d8e1bc16a163d33481e645d59
ciaooo55 <143322433+ciaooo55@users.noreply.github.com>
2023-11-13 17:28:18 +0800

    Update 123.js

--- rules/123.js
+++ rules/123.js
@@ -13,21 +13,18 @@
 hostname = api-chat.soulapp.cn, api-pay.soulapp.cn, api-a.soulapp.cn
 
 *******************************/
-// 获取主机名
-var host = $request.headers.Host;
-
-// 判断主机名
-if (host.includes('api-chat') || host.includes('api-pay') || host.includes('api-a')) {
-    $response.body = $response.body
-        .replace(/"superVIP":false/g, '"superVIP":true')
-        .replace(/"validTime":null/g, '"validTime":4567891456000')
-        .replace(/"hasFlyPackage":false/g, '"hasFlyPackage":true')
-        .replace(/"limit":true/g, '"limit":false')
-        .replace(/"currentSpeed":\d+/g, '"currentSpeed":0')
-        .replace(/"speedupTime":\d+/g, '"speedupTime":0')
-        .replace(/"speedupCount":1/g, '"speedupCount":0')
-        .replace(/"speedup":false/g, '"speedup":true');
-}
-
-$done({});
+var objc = JSON.parse($response.body);
+    objc = {
+ "data": {
+ "limit":false,
+  "superVIP":true,
+  "validTime":4567891456000,
+  "hasFlyPackage":true,
+  "speedup":true,
+  "shareToken": ""
+ },
+ "code": 0,
+ "msg": "返回成功"
+};
+$done({body : JSON.stringify(objc)});
 
