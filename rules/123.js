3c9e6d3d1f1ad49fcf30a9bd945c69ef717c271d
ciaooo55 <143322433+ciaooo55@users.noreply.github.com>
2023-11-13 17:08:27 +0800

    Update 123.js

--- rules/123.js
+++ rules/123.js
@@ -4,16 +4,30 @@
 
 *******************************
 [rewrite_local]
-^https?:\/\/.+(\/privilege\/supervip\/status|\/show\/superVIP\/detail|\/meet\/invisible\/saveByUserId|\/special\/concern\/setting|\/chat\/history\/setting|\/privilege\/bubble\/buy|\/soul-coin\/total|\/user\/queryInvisibleSetting|\/chat\/limitInfo) url script-response-body https://raw.githubusercontent.com/ciaooo55/CLASH/main/rules/123.js
+
+^https?://(api-chat|api-pay|api-a)\.soulapp\.cn url script-response-body https://raw.githubusercontent.com/ciaooo55/CLASH/main/rules/123.js
+
 
 [mitm]
-hostname = api-pay.soulapp.cn, api-user.soulapp.cn, api-chat.soulapp.cn, 8.210.3.170, 47.96.7.91, 47.96.11.46, 47.96.20.45, 47.98.54.106, 47.98.137.249, 114.215.255.94, 118.31.7.235, 118.31.112.221, 120.55.55.115, 120.55.62.124, 121.196.197.147, 121.196.203.183
+# 对应的主机名
+hostname = api-chat.soulapp.cn, api-pay.soulapp.cn, api-a.soulapp.cn
 
 *******************************/
+// 获取主机名
+var host = $request.headers.Host;
 
-var body = $response.body.replace(/"limit":true/g,'"limit":false')
-.replace(/ "validTime":"\d+"/g,'"free_minutes":"4567891456000"').replace(/"superVIP":false/g,'"superVIP":true').replace(/"hasFlyPackage":false/g,'"hasFlyPackage":true')
-.replace(/"speedup":false/g,'"speedup":true')
-$done({ body });
+// 判断主机名
+if (host.includes('api-chat') || host.includes('api-pay') || host.includes('api-a')) {
+    $response.body = $response.body
+        .replace(/"superVIP":false/g, '"superVIP":true')
+        .replace(/"validTime":null/g, '"validTime":4567891456000')
+        .replace(/"hasFlyPackage":false/g, '"hasFlyPackage":true')
+        .replace(/"limit":true/g, '"limit":false')
+        .replace(/"currentSpeed":\d+/g, '"currentSpeed":0')
+        .replace(/"speedupTime":\d+/g, '"speedupTime":0')
+        .replace(/"speedupCount":1/g, '"speedupCount":0')
+        .replace(/"speedup":false/g, '"speedup":true');
+}
 
+$done({});
 
